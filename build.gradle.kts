3234e41ad2f9c7c1cefd5415ec81ace835363df0
Vineeth Kumar R <vineeth.r@idfcbank.com>
2023-11-27 16:28:33 +0530

    Vineeth|Sitikanta [001] Code coverage check in git workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
 	id("org.flywaydb.flyway") version "9.22.3"
+	id("jacoco")
 }
 
 group = "com.project"
@@ -28,4 +29,16 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			limit {
+				counter = "LINE"
+				value = "COVEREDRATIO"
+				minimum = "0.8".toBigDecimal()
+			}
+		}
+	}
 }
