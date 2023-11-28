a132726ae361977f155aebf04b6c654edbf9acc6
gourav.sharma6 <gourav.sharma6@idfcbank.com>
2023-11-28 14:25:49 +0530

    Gourav|Kunj Commited for Test cases

--- src/test/java/com/project/bookstore/controller/BooksControllerTest.java
+++ src/test/java/com/project/bookstore/controller/BooksControllerTest.java
@@ -2,7 +2,6 @@ package com.project.bookstore.controller;
 
 import com.project.bookstore.BookService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,10 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,10 +20,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BooksControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @MockBean
-    BookService bookService;
+    private BookService bookService;
 
 
     @Test
