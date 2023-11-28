060b998b975d0b71d4a47cffa7b015788d28c8e7
chandan <chandangupta2@idfcbank.com>
2023-11-28 14:20:47 +0530

    022: Chandan|Ayushi:- Fixed DB issue to load data into database

--- src/main/java/com/project/bookstore/entity/BooksEntity.java
+++ src/main/java/com/project/bookstore/entity/BooksEntity.java
@@ -1,5 +1,6 @@
 package com.project.bookstore.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -7,95 +8,25 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "BOOK_DETAILS")
 public class BooksEntity {
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getRating() {
-        return rating;
-    }
-
-    public void setRating(String rating) {
-        this.rating = rating;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getPublicationYear() {
-        return publicationYear;
-    }
-
-    public void setPublicationYear(String publicationYear) {
-        this.publicationYear = publicationYear;
-    }
-
-    public Integer getBooksAvailable() {
-        return booksAvailable;
-    }
-
-    public void setBooksAvailable(Integer booksAvailable) {
-        this.booksAvailable = booksAvailable;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 
     @Id
-    public Integer isbn;
+    Integer isbn;
 
-    public Integer getIsbn() {
-        return isbn;
-    }
+    String author;
 
-    public void setIsbn(Integer isbn) {
-        this.isbn = isbn;
-    }
-
-    public String author;
-
-    public String name;
+    String name;
 
 
-    public String imageUrl;
+    String imageUrl;
 
-    public Double price;
+    Double price;
 
-    public Integer booksAvailable;
+    Integer booksAvailable;
 
-    public String publicationYear;
+    String publicationYear;
 
-    public String description;
+    String description;
 
-    public String rating;
+    Integer rating;
 }
+
