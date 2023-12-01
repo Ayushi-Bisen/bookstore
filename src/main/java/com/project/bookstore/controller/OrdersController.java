ea594600fe3f578e834076b1499203a1639f0ae1
Vineeth Kumar R <vineeth.r@idfcbank.com>
2023-12-01 12:21:20 +0530

    [000] Vineeth|Ayushi Refactored error response

--- src/main/java/com/project/bookstore/controller/OrdersController.java
+++ src/main/java/com/project/bookstore/controller/OrdersController.java
@@ -6,6 +6,7 @@ import com.project.bookstore.exceptions.BookOutOfStockException;
 import com.project.bookstore.request.BuyRequest;
 import com.project.bookstore.response.BuyResponse;
 import com.project.bookstore.service.OrderService;
+import com.project.bookstore.util.ErrorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +25,7 @@ public class OrdersController {
     @PostMapping(value = "/order")
     public ResponseEntity<?> createOrder(@RequestAttribute("username") String username, @RequestBody BuyRequest buyRequest) {
         if (username == null || username.equals("")) {
-            String body =  "{\"errCode\": \"INTERNAL_SERVER_ERROR\", \"message\":\"username expected in context\"}";
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+            return ErrorUtil.getInternalServerErrorResponse("username expected in context");
         }
         try {
             BuyResponse buyResponse = orderService.order(username, buyRequest);
@@ -42,16 +42,14 @@ public class OrdersController {
     @GetMapping(value = "/orders")
     public ResponseEntity<?> getOrders(@RequestAttribute("username") String username) {
         if (username == null || username.equals("")) {
-            String body =  "{\"errCode\": \"INTERNAL_SERVER_ERROR\", \"message\":\"username expected in context\"}";
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+            return ErrorUtil.getInternalServerErrorResponse("username expected in context");
         }
 
         try {
             List<Order> orders = orderService.getOrders(username);
             return ResponseEntity.ok(new Orders(orders));
         } catch (Exception ex) {
-            String body =  "{\"errCode\": \"INTERNAL_SERVER_ERROR\", \"message\":\"" + ex.getMessage() + "\"}";
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+            return ErrorUtil.getInternalServerErrorResponse(ex.getMessage());
         }
     }
 
